18d689f5f572531c087c75de186e42386337c32b
DIVAKAR RAJPUT <114133300+Divakar-Rajput@users.noreply.github.com>
2024-11-08 20:25:23 +0530

    Add files via upload

--- js/main.js
+++ js/main.js
@@ -28,3 +28,19 @@ marquee.addEventListener("mouseover", function() {
 marquee.addEventListener("mouseout", function() {
     marquee.start();
 });
+
+
+
+let currentIndex = 0;
+
+        function moveSlide(direction) {
+            const slides = document.querySelector('.slides');
+            const totalSlides = document.querySelectorAll('.slide').length;
+            currentIndex = (currentIndex + direction + totalSlides) % totalSlides;
+            slides.style.transform = `translateX(-${currentIndex * 100}%)`;
+        }
+
+        // Auto slide every 5 seconds
+        setInterval(() => {
+            moveSlide(1);
+        }, 5000);
